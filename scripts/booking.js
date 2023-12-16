5c71fa219ae019b67192e48ecbe70ce37b3422e9
MaximilianBradford <maximilian.bradford@edu.sait.ca>
2023-12-15 18:16:35 -0700

    Minor updates in order to validate

--- scripts/booking.js
+++ scripts/booking.js
@@ -2,9 +2,9 @@
 // useful variables might be: the cost per day, the number of days selected, and elements on the screen that will be clicked or will need to be modified. 
 // Do any of these variables need to be initialized when the page is loaded? 
 // When do they need to be reset or updated?
-let dailycost =  0
-let dayCounter = 0
-let calccost = 0
+let dailycost =  0;
+let dayCounter = 0;
+let calccost = 0;
 const monday = document.getElementById("monday");
 const tuesday = document.getElementById("tuesday");
 const wednesday = document.getElementById("wednesday");
@@ -13,7 +13,7 @@ const friday = document.getElementById("friday");
 const clearbutton = document.getElementById("clear-button");
 const full = document.getElementById("full");
 const half = document.getElementById("half");
-const weeklycost = document.getElementById("calculated-cost")
+const weeklycost = document.getElementById("calculated-cost");
 const list = [monday, tuesday, wednesday, thursday, friday];
 
 
@@ -25,33 +25,33 @@ const list = [monday, tuesday, wednesday, thursday, friday];
 // document.addEventListener("DOMContentLoaded", function(event) {
 
 monday.addEventListener('click', function(){
-    monday.classList.add("clicked")
-    daycounterfunc()
+    monday.classList.add("clicked");
+    daycounterfunc();
 });
 tuesday.addEventListener('click', function(){
-    tuesday.classList.add("clicked")
-    daycounterfunc()
+    tuesday.classList.add("clicked");
+    daycounterfunc();
 });
 wednesday.addEventListener('click', function(){
-    wednesday.classList.add("clicked")
-    daycounterfunc()
+    wednesday.classList.add("clicked");
+    daycounterfunc();
 });
 thursday.addEventListener('click', function(){
-    thursday.classList.add("clicked")
-    daycounterfunc()
+    thursday.classList.add("clicked");
+    daycounterfunc();
 });
 friday.addEventListener('click', function(){
-    friday.classList.add("clicked")
-    daycounterfunc()
+    friday.classList.add("clicked");
+    daycounterfunc();
 });
 function daycounterfunc(){
-    dayCounter = 0
+    dayCounter = 0;
     for (let i = 0; i <= 4; i++){
         if ((list[i].classList.contains("clicked")) == true){
             // console.log(list[i])
-            dayCounter++ 
+            dayCounter++ ;
             // console.log("round")    
-        };
+        }
     // console.log(dayCounter)
     }
 }
@@ -62,13 +62,13 @@ function daycounterfunc(){
 clearbutton.addEventListener('click', function(){
     for (let i = 0; i <= 4; i++){
         if ((list[i].classList.contains("clicked")) == true){
-            list[i].classList.remove("clicked")   
-        };
+            list[i].classList.remove("clicked") ;  
+        }
     }
-    dailycost = 0
-    dayCounter= 0
-    calccost = 0
-    recalculate()
+    dailycost = 0;
+    dayCounter= 0;
+    calccost = 0;
+    recalculate();
 });
 
 
@@ -76,20 +76,20 @@ clearbutton.addEventListener('click', function(){
 // when the half-day button is clicked, set the daily rate to $20, add the "clicked" class to the "half" element, remove it from the "full" element, and recalculate the total cost.
 
 half.addEventListener('click', function(){
-    dailycost = 20
-    half.classList.add("clicked")
-    full.classList.remove("clicked")
-    recalculate()
+    dailycost = 20;
+    half.classList.add("clicked");
+    full.classList.remove("clicked");
+    recalculate();
 });
 
 
 
 // when the full-day button is clicked, the daily rate is set back to $35, the clicked class is added to "full" and removed from "half", and the total cost is recalculated.
 full.addEventListener('click', function(){
-    dailycost = 35
-    full.classList.add("clicked")
-    half.classList.remove("clicked")
-    recalculate()
+    dailycost = 35;
+    full.classList.add("clicked");
+    half.classList.remove("clicked");
+    recalculate();
 });
 
 
@@ -98,8 +98,8 @@ full.addEventListener('click', function(){
 /********* calculate *********/
 // when a calculation is needed, set the innerHTML of the calculated-cost element to the appropriate value
 function recalculate(){
-    calccost = dayCounter*dailycost
-    console.log(calccost)
-    weeklycost.innerHTML = calccost
+    calccost = dayCounter*dailycost;
+    console.log(calccost);
+    weeklycost.innerHTML = calccost;
 };
 
